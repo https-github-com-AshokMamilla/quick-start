86d40b2d419340253d95715d1ed8287b4af41db6
MamillaMadhu <mamillaazdevops.outlook.com>
2022-10-08 14:43:40 +0530

    quick-start first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 5
+	b := 4
+	//you should use fmt.capital letter for any function in fmt or any other package
+	//fmt.println(a + b)
+	fmt.Println(a - b)
+
+}
