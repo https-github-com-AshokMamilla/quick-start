f6e372452a67d300e8183052041c10df0d5e64ea
MamillaMadhu <mamillaazdevops.outlook.com>
2022-10-08 17:18:24 +0530

    quick-start Second commit

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
-	a := 5
-	b := 4
+	//a := 5
+	//b := 4
 	//you should use fmt.capital letter for any function in fmt or any other package
 	//fmt.println(a + b)
-	fmt.Println(a - b)
+	var a int
+	var b int
+	var c int
 
+	fmt.Print("what is the value a: ")
+	//fmt.Scanf("" & a)
+	fmt.Scan(&a)
+	fmt.Print("what is the value b: ")
+	//fmt.Scanf(&b)
+	fmt.Scan(&b)
+	c = a + b
+	//fmt.Scan(&c)
+	//fmt	.Print("value of c")
+	fmt.Print("value of c=", c)
+	//fmt.Scan(&c)
+	//fmt.Scan(&c)
 }
